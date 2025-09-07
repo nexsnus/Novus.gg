91c3307bc5b4bd4ed48ae5dce3ed15c16341a32e
nexsnus <Nexusfnbr@proton.me>
2025-09-07 20:53:58 +0200

    Create BeeRichmain.lua

---
+++ BeeRich/BeeRichmain.lua
@@ -0,0 +1,35 @@
+local player = game.Players.LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local remote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("Events"):WaitForChild("Request")
+local args = {"ManualGather"}
+
+local Window = _G.Window
+
+local beraftm = Window:CreateTab({
+    Name = "Auto Farm",
+    Icon = "double_arrow",
+    ImageSource = "Material",
+    ShowTitle = true
+})
+
+local farmloop
+
+local beraft = beraftm:CreateToggle({
+    Name = "Auto Farm",
+    Description = nil,
+    CurrentValue = false,
+    Callback = function(state)
+      if state then
+        farmLoop = task.spawn(function()
+            while farmActive do
+                for i = 1, 5 do 
+                    remote:FireServer(unpack(args))
+                    remote:FireServer(unpack(args))
+                    remote:FireServer(unpack(args))
+                    remote:FireServer(unpack(args))
+                end
+                task.wait(0.01)
+            end
+        end)
+    end
+}, "beraft")
