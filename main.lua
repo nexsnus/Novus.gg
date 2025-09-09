4e20d89f4b3661b2ceeba56d66c5e541c40d32ab
nexsnus <Nexusfnbr@proton.me>
2025-09-09 18:43:38 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -27,39 +27,6 @@ _G.Window = _G.Luna:CreateWindow({
     }
 })
 
-local Window = _G.Window
-
-local ut = Window:CreateTab({
-    Name = "Universal",
-    Icon = "all_inclusive",
-    ImageSource = "Material",
-    ShowTitle = true
-})
-
-local antiAfkEnabled = false
-local antiAfkConnection
-
-local utaafkt = ut:CreateToggle({
-    Name = "Anti Afk",
-    Description = nil,
-    Callback = function()
-        if not antiAfkEnabled then
-            antiAfkEnabled = true
-            local VirtualUser = game:GetService("VirtualUser")
-            antiAfkConnection = game:GetService("Players").LocalPlayer.Idled:Connect(function()
-                VirtualUser:CaptureController()
-                VirtualUser:ClickButton2(Vector2.new())
-            end)
-        else
-            antiAfkEnabled = false
-            if antiAfkConnection then
-                antiAfkConnection:Disconnect()
-                antiAfkConnection = nil
-            end
-        end
-    end
-}, "utaafkt")
-
 if game.PlaceId == 7305309231 then
     repeat task.wait() until _G.Window
     loadstring(game:HttpGet("https://raw.githubusercontent.com/nexsnus/NovusHub/refs/heads/main/TaxiBoss/Taxibossmain.lua"))()
