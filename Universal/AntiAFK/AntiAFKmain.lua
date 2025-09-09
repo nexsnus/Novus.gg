d4e3cf7ef062b5f787c1e30e6048827413ddc0b1
nexsnus <Nexusfnbr@proton.me>
2025-09-09 18:44:22 +0200

    Update AntiAFKmain.lua

--- Universal/AntiAFK/AntiAFKmain.lua
+++ Universal/AntiAFK/AntiAFKmain.lua
@@ -1,1 +1,23 @@
+local antiAfkEnabled = false
+local antiAfkConnection
 
+local utaafkt = ut:CreateToggle({
+    Name = "Anti Afk",
+    Description = nil,
+    Callback = function()
+        if not antiAfkEnabled then
+            antiAfkEnabled = true
+            local VirtualUser = game:GetService("VirtualUser")
+            antiAfkConnection = game:GetService("Players").LocalPlayer.Idled:Connect(function()
+                VirtualUser:CaptureController()
+                VirtualUser:ClickButton2(Vector2.new())
+            end)
+        else
+            antiAfkEnabled = false
+            if antiAfkConnection then
+                antiAfkConnection:Disconnect()
+                antiAfkConnection = nil
+            end
+        end
+    end
+}, "utaafkt")
