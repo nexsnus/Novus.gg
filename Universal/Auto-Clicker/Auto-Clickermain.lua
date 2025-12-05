9dc8a981f71a605968b5301776012ceeac9e320c
nexsnus <Nexusfnbr@proton.me>
2025-12-05 18:13:27 +0100

    Update Auto-Clickermain.lua

--- Universal/Auto-Clicker/Auto-Clickermain.lua
+++ Universal/Auto-Clicker/Auto-Clickermain.lua
@@ -10,17 +10,19 @@ local Mouse = Player:GetMouse()
 local X, Y = 1700, 600
 local interval = 1
 
+while Enabled do
+      VirtualInputManager:SendMouseButtonEvent(X, Y, 0, true, game, 1)
+      VirtualInputManager:SendMouseButtonEvent(X, Y, 0, false, game, 1)
+      wait(interval)
+end
+
 local utacs = ut:CreateSection("Auto Clicker")
 local utact = ut:CreateToggle({
     Name = "Auto Clicker",
     CurrentValue = false,
     Flag = "utact",
     Callback = function(state)
-        while state do
-            VirtualInputManager:SendMouseButtonEvent(X, Y, 0, true, game, 1)
-            VirtualInputManager:SendMouseButtonEvent(X, Y, 0, false, game, 1)
-            wait(interval)
-         end
+        Enabled = state
     end,
 })
 local utacis = ut:CreateSlider({
@@ -33,4 +35,13 @@ local utacis = ut:CreateSlider({
    Callback = function(value)
         interval = value
    end,
+})
+local utactb = ut:CreateKeybind({
+        Name = "Auto Clicker Toggle Bind",
+        CurrentKeybind = "F",
+        HoldToInteract = false,
+        Flag = "utactb",
+        Callback = function()
+            Enabled = true
+        end,
 })
