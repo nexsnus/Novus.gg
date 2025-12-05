6bafdd6afb0145b1f0826bfa3c8a217f7a90f03d
nexsnus <Nexusfnbr@proton.me>
2025-12-05 18:49:40 +0100

    Update Universalmain.lua

--- Universal/Universalmain.lua
+++ Universal/Universalmain.lua
@@ -3,3 +3,4 @@ local Window = _G.Window
 _G.ut = Window:CreateTab("Universal", "infinity")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nexsnus/Novus.gg/refs/heads/main/Universal/AntiAFK/AntiAFKmain.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nexsnus/Novus.gg/refs/heads/main/Universal/Auto-Clicker/Auto-Clickermain.lua"))()
