60624e136426b5d9ab5162103ad2aa0e8d42f4ab
nexsnus <Nexusfnbr@proton.me>
2025-12-05 15:33:11 +0100

    Implement auto clicker functionality in Lua script

---
+++ Universal/Auto-Clicker/Auto-Clickermain.lua
@@ -0,0 +1,35 @@
+local Window = _G.Window
+local ut = _G.ut
+
+local CoreGui = game:GetService("CoreGui")
+local Players = game:GetService("Players")
+local VirtualInputManager = game:GetService("VirtualInputManager")
+local Player = Players.LocalPlayer
+local Enabled = false
+local Mouse = Player:GetMouse()
+local X, Y = 1700, 600
+local interval = 1
+
+local utacs = ut:CreateSection("Auto Clicker")
+local utact = ut:CreateToggle({
+    Name = "Auto Clicker",
+    CurrentValue = false,
+    Flag = "utact",
+    Callback = function(state)
+        while state do
+            VirtualInputManager:SendMouseButtonEvent(X, Y, 0, true, game, 1)
+            VirtualInputManager:SendMouseButtonEvent(X, Y, 0, false, game, 1)
+            wait(interval)
+    end,
+})
+local utacis = ut:CreateSlider({
+   Name = "Auto Clicker Interval",
+   Range = {0.01, 2},
+   Increment = 0.01,
+   Suffix = "Seconds",
+   CurrentValue = 1,
+   Flag = "utacis", 
+   Callback = function(value)
+        interval = value
+   end,
+})
