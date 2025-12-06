8ab908aa0deea689dda0324563bfbdb39b4039df
nexsnus <Nexusfnbr@proton.me>
2025-12-06 22:54:29 +0100

    Update Auto-Clickermain.lua

--- Universal/Auto-Clicker/Auto-Clickermain.lua
+++ Universal/Auto-Clicker/Auto-Clickermain.lua
@@ -4,15 +4,16 @@ local ut = _G.ut
 local CoreGui = game:GetService("CoreGui")
 local Players = game:GetService("Players")
 local VirtualInputManager = game:GetService("VirtualInputManager")
+local UserInputService = game:GetService("UserInputService")
 local Player = Players.LocalPlayer
-local Enabled = false
 local Mouse = Player:GetMouse()
+
+local Enabled = false
 local X, Y = 0, 0
 local interval = 1
-local UserInputService = game:GetService("UserInputService")
-local Players = game:GetService("Players")
 
 local utacs = ut:CreateSection("Auto Clicker")
+
 local utact = ut:CreateToggle({
     Name = "Auto Clicker",
     CurrentValue = false,
@@ -26,45 +27,45 @@ local utact = ut:CreateToggle({
         end
     end,
 })
+
 local utacis = ut:CreateSlider({
-   Name = "Auto Clicker Interval",
-   Range = {0.01, 2},
-   Increment = 0.01,
-   Suffix = "Seconds",
-   CurrentValue = 1,
-   Flag = "utacis", 
-   Callback = function(value)
+    Name = "Auto Clicker Interval",
+    Range = {0.01, 2},
+    Increment = 0.01,
+    Suffix = "Seconds",
+    CurrentValue = 1,
+    Flag = "utacis",
+    Callback = function(value)
         interval = value
-   end,
+    end,
 })
+
 local utactb = ut:CreateKeybind({
-        Name = "Auto Clicker Toggle Bind",
-        CurrentKeybind = "F",
-        HoldToInteract = false,
-        Flag = "utactb",
-        Callback = function()
-            if Enabled == true then
-                utact:Set(false)
-            else
-                utact:Set(true)
-            end
-        end,
+    Name = "Auto Clicker Toggle Bind",
+    CurrentKeybind = "F",
+    HoldToInteract = false,
+    Flag = "utactb",
+    Callback = function()
+        utact:Set(not Enabled)
+    end,
 })
+
+local utacxcpg = ut:CreateParagraph({Title = "Current X-Coordinate", Content = X})
+local utacycpg = ut:CreateParagraph({Title = "Current Y-Coordinate", Content = Y})
+
 local utacpb = ut:CreateButton({
     Name = "Set Position on Click",
     Callback = function()
         local function onMouseDown(input)
             if input.UserInputType == Enum.UserInputType.MouseButton1 then
                 X, Y = Mouse.X, Mouse.Y
-                if MouseDownConnection then
-                    MouseDownConnection:Disconnect()
+                utacxcpg:Set({Content = X})
+                utacycpg:Set({Content = Y})
+                if mouseDownConnection then
+                    mouseDownConnection:Disconnect()
                 end
             end
         end
-        local MouseDownConnection = UserInputService.InputBegan:Connect(onMouseDown)
-        utacxcpg:Set({Title = "Current X-Coordinate", Content = X})
-        utacycpg:Set({Title = "Current Y-Coordinate", Content = Y})
+        local mouseDownConnection = UserInputService.InputBegan:Connect(onMouseDown)
     end,
-})
-local utacxcpg = ut:CreateParagraph({Title = "Current X-Coordinate", Content = X})
-local utacycpg = ut:CreateParagraph({Title = "Current Y-Coordinate", Content = Y})
+})   
