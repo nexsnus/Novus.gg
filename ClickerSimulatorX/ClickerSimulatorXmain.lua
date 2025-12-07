224ebc9fab97135b2c79fe42eb812c2149d18c36
nexsnus <Nexusfnbr@proton.me>
2025-12-07 11:11:16 +0100

    Update ClickerSimulatorXmain.lua

--- ClickerSimulatorX/ClickerSimulatorXmain.lua
+++ ClickerSimulatorX/ClickerSimulatorXmain.lua
@@ -29,3 +29,103 @@ local csxmatakbt = csxmat:CreateToggle({
         end
     end,
 })   
+
+local csxmmt = Window:CreateTab("Miscellaneous", "credit-card")
+
+local csxmmtguis = cxsmmt:CreateSection("Guis")
+local cxsmmtogmt = cxsmmt:CreateToggle({
+        Name = "Open Gold Machine",
+        CurrentValue = false,
+        Flag = "cxsmmtogmt",
+        Callback = function(state)
+            if state then
+                game:GetService("Players").LocalPlayer.PlayerGui.GoldMachine.Enabled = true
+            else
+                game:GetService("Players").LocalPlayer.PlayerGui.GoldMachine.Enabled = false
+            end
+        end,
+})
+local cxsmmtocmt = cxsmmt:CreateToggle({
+        Name = "Open Cosmic Machine",
+        CurrentValue = false,
+        Flag = "cxsmmtocmt",
+        Callback = function(state)
+            if state then
+                game:GetService("Players").LocalPlayer.PlayerGui.CosmicMachine.Enabled = true
+            else
+                game:GetService("Players").LocalPlayer.PlayerGui.CosmicMachine.Enabled = false
+            end
+        end,
+})
+local cxsmmtoctmt = cxsmmt:CreateToggle({
+        Name = "Open Craft Machine",
+        CurrentValue = false,
+        Flag = "cxsmmtoctmt",
+        Callback = function(state)
+            if state then
+                game:GetService("Players").LocalPlayer.PlayerGui.CraftMachine.Enabled = true
+            else
+                game:GetService("Players").LocalPlayer.PlayerGui.CraftMachine.Enabled = false
+            end
+        end,
+})
+local cxsmmtodct = cxsmmt:CreateToggle({
+        Name = "Open Daycare",
+        CurrentValue = false,
+        Flag = "cxsmmtodct",
+        Callback = function(state)
+            if state then
+                game:GetService("Players").LocalPlayer.PlayerGui.Daycare.Enabled = true
+            else
+                game:GetService("Players").LocalPlayer.PlayerGui.Daycare.Enabled = false
+            end
+        end,
+})
+local cxsmmtofmt = cxsmmt:CreateToggle({
+        Name = "Open Forge Machine",
+        CurrentValue = false,
+        Flag = "cxsmmtofmt",
+        Callback = function(state)
+            if state then
+                game:GetService("Players").LocalPlayer.PlayerGui.ForgeMachine.Enabled = true
+            else
+                game:GetService("Players").LocalPlayer.PlayerGui.ForgeMachine.Enabled = false
+            end
+        end,
+})
+local cxsmmtormt = cxsmmt:CreateToggle({
+        Name = "Open Rainbow Machine",
+        CurrentValue = false,
+        Flag = "cxsmmtormt",
+        Callback = function(state)
+            if state then
+                game:GetService("Players").LocalPlayer.PlayerGui.RainbowMachine.Enabled = true
+            else
+                game:GetService("Players").LocalPlayer.PlayerGui.RainbowMachine.Enabled = false
+            end
+        end,
+})
+local cxsmmtopit = cxsmmt:CreateToggle({
+        Name = "Open Pet Index",
+        CurrentValue = false,
+        Flag = "cxsmmtopit",
+        Callback = function(state)
+            if state then
+                game:GetService("Players").LocalPlayer.PlayerGui.PetIndex.Enabled = true
+            else
+                game:GetService("Players").LocalPlayer.PlayerGui.PetIndex.Enabled = false
+            end
+        end,
+})
+local cxsmmtdrguit = cxsmmt:CreateToggle({
+        Name = "Disable Rebirth Gui",
+        CurrentValue = false,
+        Flag = "cxsmmtdrguit",
+        Callback = function(state)
+            if state then
+                game:GetService("Players").LocalPlayer.PlayerGui.Rebirth.Enabled = false
+            else
+                game:GetService("Players").LocalPlayer.PlayerGui.Rebirth.Enabled = true
+            end
+        end,
+})
